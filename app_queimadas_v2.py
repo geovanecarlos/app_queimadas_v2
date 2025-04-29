17abd38419c9d2f4ceb405a07371a7a718f5c355
geovane <geovanecarlos.miguel@gmail.com>
2025-04-29 18:37:12 -0300

    first commit

--- app_queimadas_v2.py
+++ app_queimadas_v2.py
@@ -265,20 +265,20 @@ with tab2:
 with tab3:
     # Plotagem do MAPA
     def plot_mapa():
-        # Convertendo o contorno do município para o formato GeoJSON
-        lim_itajuba_geojson = lim_itajuba.__geo_interface__
-        
+
+
         # Criando o mapa utilizando o Folium
         map = folium.Map(location=[ -22.44, -45.40], zoom_start=11.0)
 
-        folium.TileLayer(
-            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            attr='Esri',
-            name='Esri Satellite',
-            overlay=False,
-            control=True
-        ).add_to(map)
+        # folium.TileLayer(
+        #     tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        #     attr='Esri',
+        #     name='Esri Satellite',
+        #     overlay=False,
+        #     control=True
+        # ).add_to(map)
 
+        # Convertendo o contorno do município para o formato GeoJSON
         lim_itajuba_geojson = lim_itajuba.__geo_interface__
         folium.GeoJson(lim_itajuba_geojson, name="Limites de Itajubá/MG", style_function=lambda x: {'color': 'black', 'weight': 2, 'fillOpacity': 0}).add_to(map)
 
@@ -288,31 +288,31 @@ with tab3:
         # Adicionando o HeatMap no mapa
         HeatMap(heat_data, radius=10, name="Mapa de Calor", blur=10).add_to(map)
 
-        # Adicionando marcadores
-        marker_group = folium.FeatureGroup(name="Focos de Queimadas")
+        # # Adicionando marcadores
+        # marker_group = folium.FeatureGroup(name="Focos de Queimadas")
 
-        for idx, row in df_2019_2022.iterrows():
-            popup_text = f"""
-            <b>Endereço:</b> {row['Endereço']}<br>
-            <b>Data:</b> {row.get('Data', 'Sem Data')}<br>   
-            <b>lat:</b> {row['latitude']}<br>
-            <b>lon:</b> {row['longitude']}        
-            """
+        # for idx, row in df_2019_2022.iterrows():
+        #     popup_text = f"""
+        #     <b>Endereço:</b> {row['Endereço']}<br>
+        #     <b>Data:</b> {row.get('Data', 'Sem Data')}<br>   
+        #     <b>lat:</b> {row['latitude']}<br>
+        #     <b>lon:</b> {row['longitude']}        
+        #     """
             
-            folium.Marker(
-                location=[row['latitude'], row['longitude']],
-                popup=folium.Popup(popup_text, max_width=300),
-                icon=folium.Icon(color="red", icon="fire", icon_color="white")
-            ).add_to(marker_group)
+        #     folium.Marker(
+        #         location=[row['latitude'], row['longitude']],
+        #         popup=folium.Popup(popup_text, max_width=300),
+        #         icon=folium.Icon(color="red", icon="fire", icon_color="white")
+        #     ).add_to(marker_group)
 
-        # Adicionando o grupo de marcadores no mapa
-        marker_group.add_to(map)
+        # # Adicionando o grupo de marcadores no mapa
+        # marker_group.add_to(map)
 
         # Adicionando o Layer Control
         folium.LayerControl(position="topright").add_to(map)
         
         # Adicionando a opção de tela cheia
-        Fullscreen().add_to(map)
+        #Fullscreen().add_to(map)
 
         # Exibindo o mapa no Streamlit
         st.subheader("Mapa de Calor dos Focos de Queimadas em Itajubá/MG - 2019-2022")
