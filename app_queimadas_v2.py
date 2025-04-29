e4a773d0850ff89b4fb32210703c340101555773
geovane <geovanecarlos.miguel@gmail.com>
2025-04-29 18:29:08 -0300

    first commit

--- app_queimadas_v2.py
+++ app_queimadas_v2.py
@@ -265,27 +265,8 @@ with tab2:
 with tab3:
     # Plotagem do MAPA
     def plot_mapa():
-
         # Criando o mapa utilizando o Folium
         map = folium.Map(location=[ -22.44, -45.40], zoom_start=11.0)
-
-        # Adicionando a opção de tela cheia
-        Fullscreen().add_to(map)
-
-        # Adicionando o Layer Control
-        folium.LayerControl(position="topright").add_to(map)
-
-        # Convertendo o contorno do município para o formato GeoJSON
-        lim_itajuba_geojson = lim_itajuba.__geo_interface__
-        folium.GeoJson(lim_itajuba_geojson, name="Limites de Itajubá/MG", style_function=lambda x: {'color': 'black', 'weight': 2, 'fillOpacity': 0}).add_to(map)
-
-        folium.TileLayer(
-            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            attr='Esri',
-            name='Esri Satellite',
-            overlay=False,
-            control=True
-        ).add_to(map)
 
         # Adicionando marcadores
         marker_group = folium.FeatureGroup(name="Focos de Queimadas")
@@ -312,6 +293,24 @@ with tab3:
 
         # Adicionando o HeatMap no mapa
         HeatMap(heat_data, radius=10, name="Mapa de Calor", blur=10).add_to(map)
+
+        # Adicionando a opção de tela cheia
+        Fullscreen().add_to(map)
+
+        # Adicionando o Layer Control
+        folium.LayerControl(position="topright").add_to(map)
+
+        # Convertendo o contorno do município para o formato GeoJSON
+        lim_itajuba_geojson = lim_itajuba.__geo_interface__
+        folium.GeoJson(lim_itajuba_geojson, name="Limites de Itajubá/MG", style_function=lambda x: {'color': 'black', 'weight': 2, 'fillOpacity': 0}).add_to(map)
+
+        folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Satellite',
+            overlay=False,
+            control=True
+        ).add_to(map)
 
         # Exibindo o mapa no Streamlit
         st.subheader("Mapa de Calor dos Focos de Queimadas em Itajubá/MG - 2019-2022")
